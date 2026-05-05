671af568e03b1688dd6bfc65f6019fb3a611d71c
SKukreti0107 <shubhamkukreti.0107@gmail.com>
2026-05-05 12:29:56 +0000

    PWA + local deps

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/inter'
+import '@fontsource-variable/space-grotesk'
+import 'material-symbols/outlined.css'
 import './index.css'
 import App from './App.jsx'
 
